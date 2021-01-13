6927618e3d9bde44d912f68bac38faa8e3251d2b
alyssssa8 <alyssssa8@gmail.com>
2021-01-12 20:00:52 -0800

    twoSum print out

--- src/twoSum.java
+++ src/twoSum.java
@@ -2,10 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class twoSum {
-    public static void main(){
-        int target = 9;
+    public static void main(String[] args) {
+        int target = 18;
         int[] nums = {2,7,11,15};
-        twoSum(nums, target);
+        int [] result=twoSum(nums, target);
+        System.out.println(result.length);
+        for (int i = 0; i < 2; i++){
+            //System.out.println(result[i]);
+            System.out.println(i+"  kkk");
+        }
+
     }
 
 
@@ -16,10 +22,13 @@ public class twoSum {
             if (map.containsKey(target - nums[i])){
                     result[1] = i;
                     result[0] = map.get(target - nums[i]);
+                    System.out.println("1: " + result[0]+ " "+result[1]);
+
                     return result;
             }
             map.put(nums[i], i);  // key and value
         }
+        System.out.println("2: " + result);
         return result;
     }
 
