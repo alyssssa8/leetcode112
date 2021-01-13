be406e61f7dfa02c551a721df8304d4e31d70700
alyssssa8 <alyssssa8@gmail.com>
2021-01-13 14:51:13 -0800

    242. Valid Anagram updated

--- src/isAnagram.java
+++ src/isAnagram.java
@@ -7,6 +7,9 @@ public class isAnagram {
         String word2 = "bcedfg";
         boolean result = isAnagram(word1, word2);
         System.out.println(result);
+
+        isAnagram2("Keep", "Peek");
+        isAnagram2("Mother In Law", "Hitler Woman");
     }
 
     public static boolean isAnagram(String word1, String word2){
@@ -20,4 +23,28 @@ public class isAnagram {
 
         return Arrays.equals(str1, str2);
     }
+
+    public static void isAnagram2(String str1, String str2) {
+//        remove whitespace
+        String s1 = str1.replaceAll("\\s", ""); ///replaces all occurrences of "a" to "b"
+        String s2 = str2.replaceAll("\\s", "");  //single whitespace character
+        boolean status = true;
+        if (s1.length() != s2.length()) {
+            status = false;
+        } else {
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1, ArrayS2);
+        }
+        if (status) {
+            System.out.println(s1 + " and " + s2 + " are anagrams");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams");
+        }
+    }
+
+
+
 }
